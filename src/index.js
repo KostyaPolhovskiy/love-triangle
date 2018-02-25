c607305716c02606a813af0c3c8ab8a0307f361e
KostyaPolhovskiy <Kosty57115711@mail.ru>
2018-02-26 00:01:46 +0300

    SecondTask

--- src/index.js
+++ src/index.js
@@ -3,5 +3,20 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  const CYCLE_SIZE = 3;
+
+  var temp, numTriangl = 0;
+
+  for (var i = 0; i < preferences.length; i++) {
+    temp = preferences[i];
+    
+    for (var j = 0; j < CYCLE_SIZE - 1; j++) {
+      temp = preferences[temp - 1];
+    }
+    
+    if (temp == i + 1)
+      numTriangl++;
+  }
+
+return Math.floor(numTriangl / 3);
 };
